edbbc8df16609b2815eaf35fab31844603ed71bd
rockentrav <kuzmenelena3@gmail.com>
2023-06-11 18:21:55 +0300

    brand new project

--- main.js
+++ main.js
@@ -1,3 +1,48 @@
-import './style.css';
-import 'uno.css';
-import '@unocss/reset/tailwind.css';
+const DOM = (id) => document.getElementById(id);
+const appendBlockToContainer = (block, container) => container.appendChild(block);
+const randomRange = (min, max) => Math.floor(Math.random() * (max - min) + min);
+const getColorOrEmptyOnRandom = () => {
+  const isNotEmpty = Math.random() > 0.5;
+  if (isNotEmpty) {
+    return "black";
+  }
+  return "red";
+};
+const createBlockWithParamsAtPosition = (x, y, size, color) => {
+  const result = document.createElement("div");
+  if (color) {
+    result.style.backgroundColor = color;
+  }
+  console.log(x, y);
+  result.style.width = result.style.height = `${size}px`;
+  result.style.position = "absolute";
+  result.style.left = `${x}px`;
+  result.style.top = `${y}px`;
+  return result;
+};
+const BLOCK_SIZE = 50;
+const STEP_DELTA_X = BLOCK_SIZE;
+const DIMENSION = 8;
+const DIMENSION_HALF = DIMENSION / 2;
+const container = DOM("app");
+let columns = 0;
+let rows = 0;
+let xPos = 0;
+let yPos = 0;
+// while (rows-- > 0) {
+let colorsInLine = [];
+while (columns < DIMENSION_HALF) {
+  const color = getColorOrEmptyOnRandom();
+  const block = createBlockWithParamsAtPosition(xPos, yPos, BLOCK_SIZE, color);
+  colorsInLine.push(color);
+  appendBlockToContainer(block, container);
+  xPos += STEP_DELTA_X;
+  columns += 1;
+}
+// const rightHalfOffsetX = (DIMENSION / 2) * BLOCK_SIZE;
+// colorsInLine.reverse().forEach((color, index) => {
+//   const block = createBlock(BLOCK_SIZE * index + rightHalfOffsetX, yPos, BLOCK_SIZE, color);
+//   appendBlockToContainer(block);
+// });
+//   yPos += BLOCK_SIZE;
+// }
